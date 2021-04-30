77d9a94e1548488afef7f899f3ab04d4875bd5e5
LonelyTeapot <lonelyteapot@gmail.com>
2021-05-01 00:21:50 +0300

    Remove AuthService::init

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -3,18 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
-  static init() {
-    // CRICITAL BUG ON DEBUG
-    // see https://github.com/FirebaseExtended/flutterfire/issues/4756
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user == null) {
-        print('User is currently signed out!');
-      } else {
-        print('User is signed in!');
-      }
-    });
-  }
-
   static void signInWithGoogle() async {
     if (kIsWeb) {
       GoogleAuthProvider googleProvider = GoogleAuthProvider();
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'auth_service.dart';
 
 import 'home_page.dart';
 import 'util/stub.dart' if (dart.library.js) 'util/web.dart';
@@ -15,7 +14,6 @@ void main() async {
   restartFirebase();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  AuthService.init();
   runApp(SteamyNotes());
 }
 
